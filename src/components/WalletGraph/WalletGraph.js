97fe130bb4443259373793e2c10918d496bab166
Gabriel Brun <lydstyl@gmail.com>
2020-03-01 20:03:40 +0100

    thunk updateAssetsWithPrices & sotre with price ok TODO add action to add total, percent and chart data

--- src/components/WalletGraph/WalletGraph.js
+++ src/components/WalletGraph/WalletGraph.js
@@ -10,41 +10,25 @@ export const WalletGraph = () => {
     actions => actions.wallet.updateAssetsWithPrices
   );
 
-  console.log('1 assetsddd', assets);
-
-  updateAssetsWithPrices(assets);
-
-  // const setAssets = useStoreActions(actions => actions.wallet.removeAsset);
-  // const setCurrentTotal = useStoreActions(
-  //   actions => actions.wallet.setCurrentTotal
-  // );
-
-  // const [usdValues, setUsdValues] = useState([10, 20, 30]);
-  // const [labels, setLabels] = useState(['USD', 'BTC', 'ETH']);
-
-  // const [datas, setDatas] = useState({});
   const [data, setData] = useState(
     // https://www.chartjs.org/docs/latest/charts/doughnut.html
     {
       datasets: [
         {
           data: [10, 20, 40]
-          // data: usdValues
         }
       ],
 
-      // These labels appear in the legend and in the tooltips when hovering different arcs
       labels: ['USD', 'BTC', 'ETH']
-      // labels: labels
     }
   );
 
-  // useEffect(() => {}, [assets, setAssets]);
+  useEffect(() => {
+    updateAssetsWithPrices(assets);
+  }, []);
 
   return (
     <div className='wallet-graph'>
-      {/* <pre>{JSON.stringify(datas, null, 4)}</pre>
-      <pre>{JSON.stringify(data, null, 4)}</pre> */}
       <Doughnut data={data} />
     </div>
   );
--- src/store/store.js
+++ src/store/store.js
@@ -1,136 +1,5 @@
 import { createStore, action, thunk } from 'easy-peasy';
 
-const addPriceToAsset = assets => {
-  console.log('3 xxx', assets);
-
-  let totalUsdValue = 0;
-
-  fetch('https://api.coincap.io/v2/assets')
-    .then(response => {
-      return response.json();
-    })
-    .then(datas => {
-      // datas = datas.data.filter(crypto =>
-      //   Object.keys(assets).includes(crypto.symbol)
-      // );
-
-      const newAssets = {};
-      const cryptos = Object.keys(assets);
-
-      cryptos.forEach(crypto => {
-        newAssets[crypto] = {
-          balance: assets[crypto].balance,
-          usdPrice: 'non compté'
-        };
-      });
-
-      // assets = assets.map(asset => ({ ...asset, price: 'non compté' }));
-
-      // console.log(assets);
-
-      // datas = datas.data.filter;
-
-      // console.log('4', datas.data[0]); // 9 of 15 crypto in the wallet ... TODO : display 'not counted in front of the ones not counted
-
-      // // const assets = useStoreState(state => state.wallet.assets);
-      // console.log('5 aaaa', assets);
-
-      // Object.keys(assets).forEach(crypto => {
-      //   // assets[crypto].price
-      });
-
-      // return datas;
-
-      // // the one counted in the chart :
-      // //setDatas(datas);
-
-      // datas.forEach(asset => {
-      //   const symbol = asset.symbol;
-      //   const priceUsd = parseFloat(asset.priceUsd);
-      //   const balance = assets[asset.symbol].balance;
-      //   const usdValue = priceUsd * balance;
-      //   totalUsdValue += usdValue;
-      //   assets[symbol].price = priceUsd;
-      //   assets[symbol].usdValue = usdValue;
-      // });
-
-      // console.log(assets);
-
-      // const usdVals = [];
-
-      // Object.keys(assets).forEach(asset => {
-      //   assets[asset].percent = assets[asset].usdValue / totalUsdValue;
-
-      //   usdVals.push(assets[asset].usdValue);
-      // });
-
-      // setCurrentTotal(totalUsdValue); // totalUsdValue to the store
-
-      // // setLabels(Object.keys(assets));
-
-      // // setUsdValues(usdVals);
-
-      // setAssets(assets);
-      // ///////////////////////////////////////
-      // // descending order
-      // const labelsAndVals = [];
-
-      // Object.keys(assets).forEach(asset => {
-      //   labelsAndVals.push({
-      //     label: asset,
-      //     usdValue: assets[asset].usdValue
-      //   });
-      // });
-
-      // labelsAndVals.sort(function(a, b) {
-      //   if (a.usdValue > b.usdValue || a.usdValue === undefined) {
-      //     return -1;
-      //   }
-      //   if (b.usdValue > a.usdValue) {
-      //     return 1;
-      //   }
-      //   return 0;
-      // });
-
-      // const labels2 = [];
-      // const usdVals2 = [];
-      // labelsAndVals.forEach(asset => {
-      //   if (asset.usdValue === undefined) {
-      //     return;
-      //   }
-
-      //   labels2.push(asset.label);
-      //   labels2.usdVals2(asset.usdValue);
-      // });
-
-      // // setLabels(labels2);
-      // // setUsdValues(usdVals2);
-
-      // // {
-      // //   datasets: [
-      // //     {
-      // //       // data: [10, 20, 30]
-      // //       data: usdValues
-      // //     }
-      // //   ],
-
-      // //   // These labels appear in the legend and in the tooltips when hovering different arcs
-      // //   // labels: ['USD', 'BTC', 'ETH']
-      // //   labels: labels
-      // // }
-
-      // console.log(1);
-      // setData({ ...data, datasets: [{ data: usdVals2 }], labels: labels2 });
-
-      // console.log(2);
-      // console.log(data);
-      // console.log(labelsAndVals);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
-
 export const store = createStore({
   user: {},
   wallet: {
@@ -173,11 +42,29 @@ export const store = createStore({
     }),
 
     // thunks
-    updateAssetsWithPrices: thunk(async (state, payload) => {
-      console.log('2 payload', payload);
+    updateAssetsWithPrices: thunk(async (actions, payload) => {
+      let response = await fetch(`https://api.coincap.io/v2/assets`);
+      let datas = await response.json();
 
-      const updated = await addPriceToAsset(payload);
-      // actions.setProduct(updated); // 👈 dispatch local actions to update state
+      const availableAPISymbols = datas.data.map(crypto => crypto.symbol);
+
+      const cryptos = Object.keys(payload);
+
+      const newAssets = {};
+      cryptos.forEach(crypto => {
+        newAssets[crypto] = payload[crypto];
+        if (!availableAPISymbols.includes(crypto)) {
+          newAssets[crypto].usdPrice = 'non compté';
+        }
+      });
+
+      datas.data.forEach(crypto => {
+        if (cryptos.includes(crypto.symbol)) {
+          newAssets[crypto.symbol].usdPrice = parseFloat(crypto.priceUsd);
+        }
+      });
+
+      actions.setAssets(newAssets); // 👈 dispatch local actions to update state
     })
   }
 });
