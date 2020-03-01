f12ff4959f6021a7adce9a692c2835a252419973
Gabriel Brun <lydstyl@gmail.com>
2020-03-01 14:33:23 +0100

    remove asset ux improvement

--- src/components/AddAsset/AddAsset.js
+++ src/components/AddAsset/AddAsset.js
@@ -22,21 +22,40 @@ export const AddAsset = () => {
     setBalance(event.target.value);
   };
 
+  const handleAddAsset = payload => {
+    addAsset(payload);
+    setSymbol('');
+    setBalance('');
+
+    const list = document.querySelectorAll(
+      '.add-asset .symbol, .add-asset  .balance'
+    );
+
+    list.forEach(node => {
+      node.value = '';
+    });
+
+    setShowButton(false);
+  };
+
   return (
     <div className='add-asset'>
       <input
+        className='symbol'
         onChange={handleSymbolChange}
         type='text'
         placeholder='BTC or XRP or...'
       />
       <input
+        className='balance'
         onChange={handleBalanceChange}
         type='number'
         placeholder='balance'
       />
 
       {showButton && (
-        <button onClick={() => addAsset({ symbol, balance })}>Add</button>
+        // <button onClick={() => addAsset({ symbol, balance })}>Add</button>
+        <button onClick={() => handleAddAsset({ symbol, balance })}>Add</button>
       )}
     </div>
   );
