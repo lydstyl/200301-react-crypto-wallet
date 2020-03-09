8d10f051f6f0450f7181b5f07fd83fa51b2322cf
Gabriel Brun <lydstyl@gmail.com>
2020-03-09 16:50:56 +0100

    presentation and vidéo

--- src/components/PublicPage/PublicPage.jsx
+++ src/components/PublicPage/PublicPage.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
 
 export const PublicPage = () => {
-  return <h2>Présentation de l'application</h2>;
+  return (
+    <div className='presentation'>
+      <h2>Présentation de l'application</h2>
+      <div className='flexer'>
+        <div className='txt'>
+          <p>
+            L'idée ici est de faciliter la vie des investisseurs via cet outil
+            pour suivre votre portefeuille de cryptomonnaie.
+          </p>
+          <p>
+            Cette application vous permet notamment de visualiser la composition
+            et valeur votre portefeuille ainsi que son historique.
+          </p>
+        </div>
+        <iframe
+          title="Présentation de l'application"
+          width='auto'
+          height='auto'
+          src='https://www.youtube.com/embed/C5bDQDjBz6I'
+          frameBorder='0'
+          allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 };
