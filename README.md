2e90e7370ae6d7d78e7c6d97ebae5ad8d44cd350
Gabriel Brun <lydstyl@gmail.com>
2020-05-09 10:36:44 +0200

    fix doughnut responsive display

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Add your crypto assets and have a global vision of your wallet with an history.
+
+https://crypto-wallet.netlify.app/
+
+TODO
+
+- ajouter prix du btc à coté des alts et du total (x.xx BTC)
+- et idéalement dans le l'historique double courbe ?
+- mettre le % dans le camembert
--- src/components/WalletGraph/WalletGraph.js
+++ src/components/WalletGraph/WalletGraph.js
@@ -4,25 +4,27 @@ import { Doughnut } from 'react-chartjs-2';
 import { useStoreState } from 'easy-peasy';
 
 export const WalletGraph = () => {
-  let walletGraphData = useStoreState(state => state.wallet.walletGraphData);
+  let walletGraphData = useStoreState((state) => state.wallet.walletGraphData);
   walletGraphData = {
     datasets: [
       {
         data: walletGraphData.numbers,
         backgroundColor: walletGraphData.backgroundColor,
         hoverBackgroundColor: '#9c27b0',
-        hoverBorderColor: '#d05ce3'
-      }
+        hoverBorderColor: '#d05ce3',
+      },
     ],
 
-    labels: walletGraphData.labels
+    labels: walletGraphData.labels,
   };
 
   return (
     <div className='wallet-graph box'>
       <h2>Graphique des cryptos dont le prix a été trouvé</h2>
 
-      <Doughnut data={walletGraphData} />
+      <div className='dughnut'>
+        <Doughnut data={walletGraphData} width='100' />
+      </div>
     </div>
   );
 };
