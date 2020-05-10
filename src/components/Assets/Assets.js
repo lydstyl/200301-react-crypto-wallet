a4a01962320f94737d851e2e8a654c8e8e6d512e
Gabriel Brun <lydstyl@gmail.com>
2020-05-10 09:06:07 +0200

    fix bug when asset is usd better

--- src/components/Assets/Assets.js
+++ src/components/Assets/Assets.js
@@ -43,8 +43,10 @@ export const Assets = () => {
       <span className='value'>
         : <span className='balance'>{asset.balance}</span> *{' '}
         {asset.usdPrice.toString().substring(0, 7)} ={' '}
-        {Math.round(asset.usdValue).toLocaleString('fr')} (
-        {asset.btcValue && asset.btcValue.toFixed(3)})
+        {Math.round(asset.usdValue).toLocaleString('fr')}{' '}
+        {asset.btcValue &&
+          `(
+        ${asset.btcValue && asset.btcValue.toFixed(3)})`}
       </span>
     </li>
   ));
