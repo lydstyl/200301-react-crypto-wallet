eb9895f749921661f0964a92ac4de1f30ea706f2
Gabriel Brun <lydstyl@gmail.com>
2020-10-25 10:11:29 +0100

    TotalInEur

--- src/components/TotalInEur/TotalInEur.jsx
+++ src/components/TotalInEur/TotalInEur.jsx
@@ -1,7 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export const TotalInEur = ({ totalUsd }) => {
-  console.log('TotalInEur -> totalUsd', totalUsd)
+  const [total, setTotal] = useState('')
 
-  return <div>TotalInEur</div>
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const response = await fetch(`https://api.exchangeratesapi.io/latest`)
+        const datas = await response.json()
+
+        const totalEur = totalUsd / datas.rates.USD
+        setTotal(totalEur.toFixed(0))
+      } catch (error) {
+        console.log('setEurPrice error', error)
+      }
+    })()
+  }, [totalUsd])
+
+  return <h3>Soit {total} €</h3>
 }
